59bd284dd525ab27a773bd97aa08e793ec79621b
Mirka Lison <mirka.lison@gmail.com>
2018-10-09 10:04:27 +0300

    new deck modal static

---
+++ src/components/newDeckModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="modal-open">
+    <div class="modal-backdrop fade show"></div>
+    <div class="modal fade show d-block" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Create new deck</h4>
+            <button type="button" class="close">
+              <span>&times;</span>
+              <span class="sr-only">Close</span>
+            </button>
+          </div>
+
+          <div class="modal-body p-3">
+            <div class="form-group">
+              <label class="control-label">Name:</label>
+              <input type="text" class="form-control" id="deck-name">
+            </div>
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" id="new">Save</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'newDeckModal',
+}
+</script>
