32966a0e05696e8bf2ec682eb1fc15779d312eaa
Mirka Lison <mirka.lison@gmail.com>
2018-10-09 10:37:28 +0300

    delete deck modal static

---
+++ src/components/deleteDeckModal.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="modal-open">
+    <div class="modal-backdrop fade show"></div>
+    <div class="modal fade show d-block" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Delete deck</h4>
+            <button type="button" class="close">
+              <span>&times;</span>
+              <span class="sr-only">Close</span>
+            </button>
+          </div>
+
+          <div class="modal-body">
+            Do you really want to delete: <strong>deck name</strong> ?
+          </div>
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary">Cancel</button>
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'deleteDeckModal',
+}
+</script>
