a4ef72094d4e30c81bd5be2b4ecce5b63685b27e
Mirka Lison <mirka.lison@gmail.com>
2018-10-19 10:26:25 +0300

    deck list

--- src/components/deckRow.vue
+++ src/components/deckRow.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="d-flex flex-column deck pt-1 pointer" @click="setActiveDeck(id)">
+  <div class="d-flex flex-column deck pt-2 pointer" @click="setActiveDeck(id)">
     <div class="d-flex">
-      <div class="col-sm-9 col-xs-7">{{name}}</div>
-      <div class="col-sm-1 col-xs-2 text-danger"><strong>{{due}}</strong></div>
-      <div class="col-sm-1 col-xs-3 text-primary">{{cards}}</div>
+      <div class="col-sm-9 col-xs-7 pb-2">{{name}}</div>
+      <div class="col-sm-1 col-xs-2 text-danger font-weight-bold text-center">{{due}}</div>
+      <div class="col-sm-1 col-xs-3 text-center">{{cards}}</div>
     </div>
-    <div class="col-sm-12 col-xs-12 py-3" v-if="isDeckExpanded">
+    <div class="col-sm-12 col-xs-12 pb-2" v-if="isDeckExpanded">
       <router-link to="/study" tag="button" class="btn btn-secondary mr-2">Study</router-link>
       <button type="button" class="btn btn-outline-secondary mr-2" @click="toggleModal('createCard')">Add card</button>
       <button type="button" class="btn btn-outline-secondary mr-2" @click="toggleModal('editDeck')">Edit</button>
