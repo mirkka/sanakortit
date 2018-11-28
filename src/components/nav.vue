65b17a137d84e11bb041788ed9fc80f0f6d53978
Mirka Lison <mirka.lison@gmail.com>
2018-11-28 16:06:42 +0200

    display logged in user name in header

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -7,26 +7,49 @@
       <router-link to="/" tag="div" class="p-3 pointer">Decks</router-link>
       <router-link to="/browse" class="pointer p-3" tag="div">Browse</router-link>
     </div>
-    <div class="col-sm-3 d-flex justify-content-end align-items-center ">
+    <div class="flex-grow-1 d-flex justify-content-end align-items-center mx-2">
       <button class="pointer logout-button"
               v-if="isNavVisible"
               @click="handleLogout">
         <i class="fa fa-sign-out-alt"></i>
       </button>
+      <div v-if="isNavVisible">{{user}}</div>
     </div>
   </div>
 </template>
 
 <script>
 import { Auth } from 'aws-amplify'
+import { getUser } from '../helpers.js'
 
 
 export default {
   name: 'navigation',
+  data () {
+    return {
+      user: ""
+    }
+  },
   computed: {
     isNavVisible() {
       const { path } = this.$route
       return path !== '/login' && path !== '/signup'
+    }
+  },
+  async updated() {
+    try {
+      const user = await getUser()
+      this.user = user
+    } catch(error) {
+      this.user = ""
+    }
+  },
+  async mounted() {
+    try {
+      const user = await getUser()
+      this.user = user
+    } catch(error) {
+      this.user = ""
     }
   },
   methods: {
