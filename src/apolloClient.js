5cb72e8ddad24f2e3a6cfe3f27bccec10e4be0f6
Mirka Lison <mirka.lison@gmail.com>
2020-12-08 15:35:21 +0200

    updated graphql setup

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -4,25 +4,24 @@ import Amplify, { Auth } from 'aws-amplify'
 import resolvers from "./graphql/resolvers"
 import defaults from "./graphql/defaults"
 import { typeDefs } from "./graphql/schema"
-import { AUTH_TYPE } from 'aws-appsync/lib/link/auth-link'
 
 import AWSAppSyncClient, { createAppSyncLink, createLinkWithCache } from "aws-appsync";
 
 Amplify.configure({
   Auth: {
-    region: process.env.VUE_APP_AWS_REGION,
-    userPoolId: process.env.VUE_APP_COGNITO_USER_POOL_ID,
-    userPoolWebClientId: process.env.VUE_APP_COGNITO_APP_ID,
+    region: 'eu-central-1',
+    userPoolId: 'eu-central-1_HrNi7unpr',
+    userPoolWebClientId: '2dh1j3c5becapg6hdq78vuff02',
   },
   disableOffline: true,
   fetchPolicy: 'no-cache'
 });
 
 const appSyncLink = createAppSyncLink({
-  url: process.env.VUE_APP_API_URL,
-  region: process.env.VUE_APP_AWS_REGION,
+  url: 'https://hx7w34zox5ftlaxn47odjdr4ri.appsync-api.eu-central-1.amazonaws.com/graphql',
+  region: 'eu-central-1',
   auth: {
-    type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    type: 'AMAZON_COGNITO_USER_POOLS',
     jwtToken: async () => (await Auth.currentSession()).getIdToken().getJwtToken(),
   },
   disableOffline: true,
